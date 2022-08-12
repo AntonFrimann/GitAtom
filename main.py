e4c2d7fea3b8c2dc1bcf16ba23ff33ade5e14322
Anton <antobuii.f@gmail.com>
2022-08-12 09:27:04 +0200

    fuck ! ! !

--- main.py
+++ main.py
@@ -4,13 +4,7 @@
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}?')  # Press Ctrl+F8 to toggle the breakpoint.
+print("Tëscht dem Bësch ass keen dat ass seng Schéinheet d'Sonn dréchent, een verschwannen dësem Hotel ass e waarmen Dram tatsächlech Notiz entstinn vum Bësch an konfus Evenementer")
 
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
